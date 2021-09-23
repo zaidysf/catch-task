d7599136e5f72dcce695e918b4d2d53d8d03786f
Zaid Yasyaf <zaid.ug@gmail.com>
2021-09-23 19:12:59 +0700

    Change UI

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Export List</title>
-</head>
-<body>
-    <table>
+@extends('layout')
+@section('content')
+    <table class="table table-striped">
         <thead>
             <tr>
                 <th>Processed Date</th>
                 <th>File</th>
+                <th>Validate</th>
                 <th>Status</th>
             </tr>
         </thead>
@@ -26,5 +20,4 @@
             @endforeach
         </tbody>
     </table>
-</body>
-</html>
+@endsection
