20f4b7aaf0deb657833ae1fca5509f1cd2d5261c
Zaid Yasyaf <zaid.ug@gmail.com>
2021-09-28 16:05:50 +0700

    Add a Proper Order Test

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -2,6 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Exports\Order\Order;
+use App\Jobs\Order\UpdateDBAfterExport;
+use App\Models\QueueExportFile;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
 use Tests\TestCase;
 
 class OrderTest extends TestCase
@@ -20,8 +25,63 @@ class OrderTest extends TestCase
 
     public function test_user_can_process_order_export()
     {
+        // Storage::fake('public');
+        $countIDB = QueueExportFile::all()->count();
+        $initStorage = Storage::disk('public')->files('/');
+        $countIS = count(array_filter($initStorage,function($a) {return str_contains($a, '.csv');}));
+
         $response = $this->get('/export');
 
         $response->assertStatus(200);
+
+        $latestDB = QueueExportFile::all()->count();
+        $latestStorage = Storage::disk('public')->files('/');
+        $countLS = count(array_filter($latestStorage,function($a) {return str_contains($a, '.csv');}));
+
+        if (
+            $countLS - $countIS == 1 &&
+            $latestDB - $countIDB == 1 &&
+            QueueExportFile::orderBy('id', 'desc')->first()->status == 1
+        ) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    public function test_user_can_process_order_export_via_console()
+    {
+        $countIDB = QueueExportFile::all()->count();
+        $initStorage = Storage::disk('public')->files('/');
+        $countIS = count(array_filter($initStorage,function($a) {return str_contains($a, '.csv');}));
+
+        $this->artisan('order:export')
+            ->expectsOutput('Export started!')
+            ->assertExitCode(0);
+
+        $latestDB = QueueExportFile::all()->count();
+        $latestStorage = Storage::disk('public')->files('/');
+        $countLS = count(array_filter($latestStorage,function($a) {return str_contains($a, '.csv');}));
+
+        if (
+            $countLS - $countIS == 1 &&
+            $latestDB - $countIDB == 1 &&
+            QueueExportFile::orderBy('id', 'desc')->first()->status == 1
+        ) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    public function test_user_can_view_export_order_list_via_console()
+    {
+        $this->artisan('order:list')
+            ->expectsTable([
+                'ID',
+                'Date',
+                'File URL',
+                'Status'
+            ], QueueExportFile::all(['id', 'processed_date', 'file_url', 'status'])->toArray());
     }
 }
